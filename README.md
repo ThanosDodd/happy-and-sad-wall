09fc79d191396b0dd58ab35f9e35ca0cdc4aba50
ThanosDodd <36003580+ThanosDodd@users.noreply.github.com>
2023-03-16 03:27:07 +0000

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# happy-and-sad-wall
-For students to know if they are in their teacher's good graces or not
---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="happy">
+        <h1>🙂</h1>
+
+        <div></div>
+      </div>
+      <div className="sad">
+        <h1>😐</h1>
+
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
